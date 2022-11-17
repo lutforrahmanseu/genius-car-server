8f93c727ee352f30290b287e90d59feb438f8478
Md.Lutfor Rahman <61869128+lutforrahmanseu@users.noreply.github.com>
2022-11-17 12:57:12 +0600

    basic node mongoDB

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+//middle ware
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.byhbjys.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("genius car server is running");
+});
+app.listen(port, () => {
+  console.log(`Genius Car server running on${port}`);
+});
